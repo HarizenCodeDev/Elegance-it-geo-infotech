d5a0e61a563d9c963d42d96cfa379ad96fd6988b
mrnobody <052005hari@gmail.com>
2026-04-18 14:39:53 +0530

    fix: export default outside if block

--- server/config/database.js
+++ server/config/database.js
@@ -4,16 +4,7 @@ import knexConfig from "../knexfile.js";
 
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL || process.env.DB_URL;
+const env = process.env.NODE_ENV || "production";
+const db = knex(knexConfig[env]);
 
-// Use knexConfig for Supabase connection
-if (connectionString && connectionString.includes("supabase.co")) {
-  const environment = process.env.NODE_ENV || "production";
-  const finalConfig = knexConfig[environment];
-  const db = knex(finalConfig);
-  export default db;
-} else {
-  const environment = process.env.NODE_ENV || "development";
-  const db = knex(knexConfig[environment]);
-  export default db;
-}
+export default db;
